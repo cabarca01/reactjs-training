a43bf9a8bfb708e76e4e0b16701bf030fea477e2
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-10-30 19:05:53 +0100

    Add year filter component

--- 01-starting-setup/src/components/expenses/ExpenseList.js
+++ 01-starting-setup/src/components/expenses/ExpenseList.js
@@ -1,19 +1,34 @@
+import React from "react";
+
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
 import Card from "../UI/Card";
 
-import "./ExpenseList.css"
+import "./ExpenseList.css";
 
 function ExpenseList({ expenses }) {
+  const [yearFilter, setYearFilter] = React.useState("");
+
+  const applyYearFilterHandler = (selectedYear) => {
+    setYearFilter(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter onApplyFilterByYear={applyYearFilterHandler} />
+
+        {expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 }
 
