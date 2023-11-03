5847fd5ef4ddf29cd5600e65e2898ecfbb83bfb1
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-03 18:54:43 +0100

    Makes PlayerSection component stateless

--- 04-tic-tac-toe/src/components/player/PlayerSection.jsx
+++ 04-tic-tac-toe/src/components/player/PlayerSection.jsx
@@ -1,17 +1,12 @@
 import "./PlayerSection.css";
-
-import { useState } from "react";
-
 import Player from "./Player";
 
 export default function PlayerSection({activePlayer}) {
-  const [player1Name, setPlayer1Name] = useState("Player 1");
-  const [player2Name, setPlayer2Name] = useState("Player 2");
-
+ 
   return (
     <ol id="players" className="highlight-player">
-      <Player key="player_1" name={player1Name} symbol={"X"} isActive={activePlayer === "X"} />
-      <Player key="player_2" name={player2Name} symbol={"O"} isActive={activePlayer === "O"} />
+      <Player key="player_1" name="Player 1" symbol={"X"} isActive={activePlayer === "X"} />
+      <Player key="player_2" name="Player 2" symbol={"O"} isActive={activePlayer === "O"} />
     </ol>
   );
 }
