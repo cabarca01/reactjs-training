b33e61fb01b24f009a929b3bfad47957f86d60d4
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-07 19:24:54 +0100

    Adds required totals tonhelper function

--- 05-investment-calculator/src/util/investment.js
+++ 05-investment-calculator/src/util/investment.js
@@ -12,15 +12,21 @@ export function calculateInvestmentResults({
 }) {
   const annualData = [];
   let investmentValue = initialInvestment;
+  let investedCapital = initialInvestment;
+  let totalInterest = 0
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
+    investedCapital += annualInvestment;
+    totalInterest += interestEarnedInYear;
+
     annualData.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
+      totalInterest: totalInterest, // Accumulated interest per year
       valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
+      investedCapital: investedCapital, // Invested capital
     });
   }
 
