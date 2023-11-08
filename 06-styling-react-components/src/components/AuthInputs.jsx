9a3c291f35e59e23b1f76f66a5126b1b388fee5d
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-08 16:18:36 +0100

    Adding styles for nested componentes using styled-components

--- 06-styling-react-components/src/components/AuthInputs.jsx
+++ 06-styling-react-components/src/components/AuthInputs.jsx
@@ -35,26 +35,49 @@ export default function AuthInputs() {
     font-weight: 700;
     letter-spacing: 0.1em;
     text-transform: uppercase;
-    color: ${({$invalid}) => ($invalid ? "#f87171" : "#6b7280")} ;
+    color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
   `;
   const ControlInput = styled.input`
     width: 100%;
     padding: 0.75rem 1rem;
     line-height: 1.5;
-    background-color: ${({$invalid}) => ($invalid ? "#fed2d2" : "#d1d5db")};
-    color: ${({$invalid}) => ($invalid ? "#ef4444" : "#374151")};
-    border: 1px solid ${({$invalid}) => ($invalid ? "#f73f3f" : "transparent")};
+    background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+    color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+    border: 1px solid
+      ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
     border-radius: 0.25rem;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  `;
+
+  const ControlButton = styled.button`
+    padding: 1rem 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 0.25rem;
+    color: #1f2937;
+    background-color: #f0b322;
+    border-radius: 6px;
+    border: none;
+
+    &:hover {
+      background-color: #f0920e;
+    }
+  `;
+
+  const ControlTextButton = styled.button`
+    color: #f0b322;
+    border: none;
+
+    &:hover {
+      color: #f0920e;
+    }
   `;
 
   return (
     <div id="auth-inputs">
       <ControlDiv>
         <p>
-          <ControlLabel $invalid={emailNotValid}>
-            Email
-          </ControlLabel>
+          <ControlLabel $invalid={emailNotValid}>Email</ControlLabel>
           <ControlInput
             $invalid={emailNotValid}
             type="email"
@@ -62,9 +85,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <ControlLabel $invalid={passwordNotValid}>
-            Password
-          </ControlLabel>
+          <ControlLabel $invalid={passwordNotValid}>Password</ControlLabel>
           <ControlInput
             $invalid={passwordNotValid}
             type="password"
@@ -75,12 +96,12 @@ export default function AuthInputs() {
         </p>
       </ControlDiv>
       <div className="actions">
-        <button type="button" className="text-button">
+        <ControlTextButton type="button" className="text-button">
           Create a new account
-        </button>
-        <button className="button" onClick={handleLogin}>
+        </ControlTextButton>
+        <ControlButton className="button" onClick={handleLogin}>
           Sign In
-        </button>
+        </ControlButton>
       </div>
     </div>
   );
