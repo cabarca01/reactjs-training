d0a3f7b08667221741aaa429596dbfffc0ed8462
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-09 15:11:52 +0100

    Styling the header with Tailwind

--- 06-styling-react-components/src/components/Header.jsx
+++ 06-styling-react-components/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import logo from "../assets/logo.png";
-import { StyledHeader } from "./styled/StyledHeader";
+/* import { StyledHeader } from "./styled/StyledHeader"; */
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="object-contain mb-8 h-44 w-44" />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
+    </header>
   );
 }
