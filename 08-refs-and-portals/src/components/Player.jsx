5d4cf4e1bf4286abf9d2e9e38dc92d428ebc0b1b
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-13 11:51:08 +0100

    Sets palyer name and clears input

--- 08-refs-and-portals/src/components/Player.jsx
+++ 08-refs-and-portals/src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerNameInput = useRef();
+  const [playerName, setPlayerName] = useState("unknown entity");
+
+  function setNameHandler() {
+    setPlayerName(playerNameInput.current.value);
+    playerNameInput.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerNameInput} />
+        <button onClick={setNameHandler}>Set Name</button>
       </p>
     </section>
   );
