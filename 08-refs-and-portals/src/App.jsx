e27d6d3a6405d2e49c012e2a606e65e0966445a7
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-15 11:46:02 +0100

    Adds multiple timer challenges to main app

--- 08-refs-and-portals/src/App.jsx
+++ 08-refs-and-portals/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" duration={1} />
+        <TimerChallenge title="Not So Easy" duration={5} />
+        <TimerChallenge title="Getting Difficult" duration={10} />
+        <TimerChallenge title="Only for Pros" duration={15} />
+      </div>
     </>
   );
 }
