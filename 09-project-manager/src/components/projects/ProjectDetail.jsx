6e701092de9c0ffb7705766f008b3970253eecc9
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-16 18:08:30 +0100

    Adds support to confirm deletion of projects

--- 09-project-manager/src/components/projects/ProjectDetail.jsx
+++ 09-project-manager/src/components/projects/ProjectDetail.jsx
@@ -1,5 +1,8 @@
+import { useRef } from "react";
 import { getDateString } from "../../utils/utils";
+
 import Tasks from "../tasks/Tasks";
+import ConfirmModal from "../UI/ConfirmModal";
 
 export default function ProjectDetails({
   project,
@@ -7,33 +10,54 @@ export default function ProjectDetails({
   onDeleteProject,
   onDeleteTask,
 }) {
-  return (
-    <div className="w-[35rem] mt-16">
-      <header className="pb-4 mb-4 border-b-2 border-stone-300">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-stone-600 mb-2">
-            {project.title}
-          </h1>
-          <button
-            className="text-stone-600 hover:text-stone-950"
-            onClick={() => {
-              onDeleteProject(project.id);
-            }}
-          >
-            Delete
-          </button>
-        </div>
-        <p className="mb-4 text-stone-400">{getDateString(project.dueDate)}</p>
-        <p className="text-stone-600 whitespace-pre-wrap">
-          {project.description}
-        </p>
-      </header>
+  const confirmRef = useRef();
 
-      <Tasks
-        taskList={project.tasks}
-        onTaskSave={onSaveTask}
-        onTaskDelete={onDeleteTask}
-      />
-    </div>
+  function deleteProjectHandler() {
+    confirmRef.current.open();
+  }
+
+  return (
+    <>
+      <ConfirmModal
+        ref={confirmRef}
+        onConfirm={() => {
+          onDeleteProject(project.id);
+        }}
+      >
+        <h2 className="text-xl font-bold text-stone-500 my-4">Warning!!</h2>
+        <p className="text-stone-400 mb-4">
+          You are about to delete the current project.
+        </p>
+        <p className="text-stone-400 mb-4">Do you want to proceed?</p>
+      </ConfirmModal>
+
+      <div className="w-[35rem] mt-16">
+        <header className="pb-4 mb-4 border-b-2 border-stone-300">
+          <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-bold text-stone-600 mb-2">
+              {project.title}
+            </h1>
+            <button
+              className="text-stone-600 hover:text-stone-950"
+              onClick={deleteProjectHandler}
+            >
+              Delete
+            </button>
+          </div>
+          <p className="mb-4 text-stone-400">
+            {getDateString(project.dueDate)}
+          </p>
+          <p className="text-stone-600 whitespace-pre-wrap">
+            {project.description}
+          </p>
+        </header>
+
+        <Tasks
+          taskList={project.tasks}
+          onTaskSave={onSaveTask}
+          onTaskDelete={onDeleteTask}
+        />
+      </div>
+    </>
   );
 }
