e41b4864882e4878f5bb4456d88937c427b42c0a
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-29 13:14:13 +0100

    Converts functional approach to classes

--- 14-Class-Components/src/components/User.js
+++ 14-Class-Components/src/components/User.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import classes from './User.module.css';
+import React from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends React.Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
 
 export default User;
