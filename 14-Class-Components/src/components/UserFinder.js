c651bed60533ed6a116fdf764a14dc5b84ae3011
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-29 16:45:09 +0100

    Implements useEffect in class component

---
+++ 14-Class-Components/src/components/UserFinder.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Users from "./Users";
+import classes from "./UserFinder.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class UserFinder extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default UserFinder;
