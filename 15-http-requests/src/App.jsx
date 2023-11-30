6b5b9056a6c622fd6c25e054dcab0d30a68bf535
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-30 18:06:53 +0100

    Adds logic to retreive and update user selected places

--- 15-http-requests/src/App.jsx
+++ 15-http-requests/src/App.jsx
@@ -1,17 +1,38 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from "react";
+import { getSelectedPlaces, updateSelectedPlaces } from "./http.js";
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
+import Places from "./components/Places.jsx";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import AvailablePlaces from "./components/AvailablePlaces.jsx";
+import ErrorMessage from "./components/ErrorMessage.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorFetchingPlaces, setErrorFetchingPlaces] = useState();
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
+  const [isFetchingPlaces, setIsFetchingPlaces] = useState(false);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function getPlaces() {
+      setIsFetchingPlaces(true);
+      try {
+        const placeList = await getSelectedPlaces();
+        setUserPlaces(placeList.places);
+      } catch (error) {
+        setErrorFetchingPlaces({
+          message: error.message || "Something went wrong getting user places!",
+        });
+      }
+      setIsFetchingPlaces(false);
+    }
+    getPlaces();
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -22,7 +43,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,8 +53,21 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      await updateSelectedPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({
+        message:
+          error.message || "Something went wrong updating selected place list!",
+      });
+    }
   }
 
+  function handleErrorModal() {
+    setErrorUpdatingPlaces(null);
+  }
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
     setUserPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
@@ -51,6 +85,16 @@ function App() {
         />
       </Modal>
 
+      <Modal open={errorUpdatingPlaces} onClose={handleErrorModal}>
+        {errorUpdatingPlaces && (
+          <ErrorMessage
+            title="Error updating selected places"
+            message={errorUpdatingPlaces.message}
+            onConfirm={handleErrorModal}
+          />
+        )}
+      </Modal>
+
       <header>
         <img src={logoImg} alt="Stylized globe" />
         <h1>PlacePicker</h1>
@@ -60,12 +104,21 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+        {errorFetchingPlaces ? (
+          <ErrorMessage
+            title="Error fetching user places"
+            message={errorFetchingPlaces.message}
+          />
+        ) : (
+          <Places
+            title="I'd like to visit ..."
+            fallbackText="Select the places you would like to visit below."
+            places={userPlaces}
+            onSelectPlace={handleStartRemovePlace}
+            isLoading={isFetchingPlaces}
+            loadingText="Retrieving user-selected places..."
+          />
+        )}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
--- 15-http-requests/src/http.js
+++ 15-http-requests/src/http.js
@@ -2,11 +2,39 @@ const server = "http://localhost:3000";
 
 export async function getAvailablePlaces() {
   const response = await fetch(server + "/places");
-  const respBody = await response.json();
-
+  
   if (!response.ok) {
     throw new Error("Could not retrieve available places!");
   }
 
+  const respBody = await response.json();
   return respBody.places;
+}
+
+export async function getSelectedPlaces (){
+    const response = await fetch(server + "/user-places");
+  
+    if (!response.ok) {
+      throw new Error("Could not retrieve user-selected places!");
+    }
+  
+    const respBody = await response.json();
+    return respBody;
+}
+
+export async function updateSelectedPlaces(placeList) {
+  const response = await fetch(server + "/user-places", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({places: placeList}),
+  });
+
+  if (!response.ok) {
+    throw new Error("Could not update selected places!");
+  }
+
+  const respBody = response.json();
+  return respBody.message; 
 }
