905818154f80cb372ba474bc9f6300aee6399566
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-30 13:11:03 +0100

    Fetches list of availale places from backend

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [placesList, setPlacesList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((respBody) => {
+        setPlacesList(respBody.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={placesList}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
