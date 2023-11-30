27cb3d5ec5a518fb8ff6ddc0d7cfc79abc10c951
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-30 14:26:30 +0100

    Adding error handling

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -1,22 +1,41 @@
 import { useEffect, useState } from "react";
 
 import Places from "./Places.jsx";
+import ErrorMessage from "./ErrorMessage.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [placesList, setPlacesList] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    async function getPlaces () {
-      setIsFetching (true);
-      const response = await fetch("http://localhost:3000/places");
-      const respBody = await response.json();
-      setPlacesList(respBody.places);
-      setIsFetching (false);
+    async function getPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const respBody = await response.json();
+        setPlacesList(respBody.places);
+
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Something failed while fetching places list!",
+        });
+      }
+
+      setIsFetching(false);
     }
 
     getPlaces();
   }, []);
+
+  if (error) {
+    return <ErrorMessage title="Error fetching data" message={error.message} />;
+  }
 
   return (
     <Places
