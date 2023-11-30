e34ce94f32e5ba128df0c1cdaa87ec47fd1a03fe
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2023-11-30 13:38:23 +0100

    Adding loading state when fetching places

--- 15-http-requests/src/components/AvailablePlaces.jsx
+++ 15-http-requests/src/components/AvailablePlaces.jsx
@@ -4,12 +4,15 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [placesList, setPlacesList] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function getPlaces () {
+      setIsFetching (true);
       const response = await fetch("http://localhost:3000/places");
       const respBody = await response.json();
       setPlacesList(respBody.places);
+      setIsFetching (false);
     }
 
     getPlaces();
@@ -21,6 +24,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
       places={placesList}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
+      isLoading={isFetching}
+      loadingText="Fetching list of places..."
     />
   );
 }
