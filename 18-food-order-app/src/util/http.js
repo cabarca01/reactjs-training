36f06ec88f2623f43ce098b18b49ca1fe040611f
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-01-17 21:14:17 +0100

    Fixes typo in error message

--- 18-food-order-app/src/util/http.js
+++ 18-food-order-app/src/util/http.js
@@ -4,7 +4,7 @@ export async function getMealList() {
   const response = await fetch(server + "/meals");
 
   if (!response.ok) {
-    throw new Error("Could not fetch llist of meals");
+    throw new Error("Could not fetch list of meals");
   }
 
   const respBody = await response.json();
