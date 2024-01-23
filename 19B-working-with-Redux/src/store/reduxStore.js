9695d3343e232c6548c3b06a876035a83d28fc3b
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-01-23 13:38:33 +0100

    Creates Redux store and provides it to the App

---
+++ 19B-working-with-Redux/src/store/reduxStore.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+
+function counterReducer(currState = { counter: 0 }, action) {
+  let newStateValue = currState.counter;
+  if (action.type === "increment") {
+    newStateValue += 1;
+  } else if (action.type === "decrement") {
+    newStateValue -= 1;
+  }
+  return {
+    counter: newStateValue,
+  };
+}
+
+const store = createStore(counterReducer);
+export default store;
