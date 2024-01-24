3d820c33bec1fb92dc76b8a0f20f27e94de1e9a3
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-01-24 18:17:09 +0100

    Fixes typo in reducers

--- 20-advanced-redux/src/components/Cart/CartButton.js
+++ 20-advanced-redux/src/components/Cart/CartButton.js
@@ -1,10 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = () => {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
+
+  function openCartHandler() {
+    dispatch(cartActions.showCart());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={openCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItems.length}</span>
     </button>
   );
 };
--- 20-advanced-redux/src/store/cart-slice.js
+++ 20-advanced-redux/src/store/cart-slice.js
@@ -16,19 +16,19 @@ const cartSlice = createSlice({
       state.isVisible = false;
     },
     addItem: (state, action) => {
-      const newItem = { ...action.payload.item, quatity: 1 };
+      const newItem = { ...action.payload.item, quantity: 1 };
       const itemIndex = state.items.findIndex((item) => item.id === newItem.id);
-      itemIndex > 0
-        ? (state.items[itemIndex].item.quatity += 1)
+      itemIndex >= 0
+        ? (state.items[itemIndex].quantity += 1)
         : state.items.push(newItem);
     },
     removeItem: (state, action) => {
       const itemIndex = state.items.findIndex(
         (item) => item.id === action.payload.id
       );
-      state.items[itemIndex].quatity === 1
+      state.items[itemIndex].quantity === 1
         ? state.items.splice(itemIndex, 1)
-        : (state.items[itemIndex].quatity -= 1);
+        : (state.items[itemIndex].quantity -= 1);
     },
   },
 });
