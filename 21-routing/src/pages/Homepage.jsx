58a0f0fd3b45a3d5ff94fef26461011566dde7f5
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-01-31 16:11:37 +0100

    Adds links tos navegate between pages

--- 21-routing/src/pages/Homepage.jsx
+++ 21-routing/src/pages/Homepage.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Homepage() {
-  return <h1>My Homepage</h1>;
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <p>
+        Go to the <Link to="/products">Products Page</Link>
+      </p>
+    </>
+  );
 }
--- 21-routing/src/pages/Products.jsx
+++ 21-routing/src/pages/Products.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
-  return <h1>The Products Page</h1>;
+  return (
+    <>
+      <h1>The Products Page</h1>{" "}
+      <p>
+        Go <Link to="/">Back</Link>
+      </p>
+    </>
+  );
 }
