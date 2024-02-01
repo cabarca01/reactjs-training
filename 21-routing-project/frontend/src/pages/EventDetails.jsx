6c7115695cf583696526bf517110e6e2fc0ecfe6
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-02-01 18:55:32 +0100

    Adds list of dummy events to test routing

--- 21-routing-project/frontend/src/pages/EventDetails.jsx
+++ 21-routing-project/frontend/src/pages/EventDetails.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
+
 export default function EventDetails() {
+  const urlParams = useParams();
   return (
     <>
-      <h1>Details for the event</h1>
+      <h1>Details for the event: {urlParams.eventId}</h1>
     </>
   );
 }
--- 21-routing-project/frontend/src/pages/Events.jsx
+++ 21-routing-project/frontend/src/pages/Events.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
+const eventList = [
+  { id: "ev1", title: "Event 1" },
+  { id: "ev2", title: "Event 2" },
+  { id: "ev3", title: "Event 3" },
+];
+
 export default function Events() {
   return (
     <>
       <h1>Events Page</h1>
+      <div>
+        <ul>
+          {eventList.map((event) => (
+            <li key={event.id}>
+              <Link to={event.id}>{event.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
