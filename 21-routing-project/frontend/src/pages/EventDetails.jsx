2457c3a8c6353869e0945fdaabe93b41fb05ace8
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-02-01 18:02:12 +0100

    Created dummy pages for content

---
+++ 21-routing-project/frontend/src/pages/EventDetails.jsx
@@ -0,0 +1,7 @@
+export default function EventDetails() {
+  return (
+    <>
+      <h1>Details for the event</h1>
+    </>
+  );
+}
---
+++ 21-routing-project/frontend/src/pages/EventForm.jsx
@@ -0,0 +1,7 @@
+export default function EventForm() {
+  return (
+    <>
+      <h1>Event Form</h1>
+    </>
+  );
+}
