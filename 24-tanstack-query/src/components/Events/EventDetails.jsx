78ac1a82cfc004e70e206f5c0a794682a85a5a7d
Carlos Augusto Abarca Bayona <cabarca01@gmail.com>
2024-02-13 17:44:55 +0100

    Implements event details and event deletion

--- 24-tanstack-query/src/components/Events/EventDetails.jsx
+++ 24-tanstack-query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,45 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  deleteEvent,
+  fetchSingleEvent,
+  queryClient,
+} from "../../utils/http.js";
+import Header from "../Header.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { eventId: id }],
+    queryFn: ({ signal }) => fetchSingleEvent({ signal, id }),
+  });
+
+  const {
+    mutate,
+    isPending: isDeletePending,
+    isError: isDeleteError,
+    error: deleteError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
+
+  function deleteHandler() {
+    if (window.confirm("Are you sure?")) {
+      mutate({ id });
+    }
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +48,44 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <p>Fetching event details...</p>}
+      {isError && (
+        <ErrorBlock
+          title="Failed to fetch event details"
+          message={error.info?.message}
+        />
+      )}
+      {isDeleteError && (
+        <ErrorBlock
+          title="Failed to delete event"
+          message={deleteError.info?.message}
+        />
+      )}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            {!isDeletePending && (
+              <nav>
+                <button onClick={deleteHandler}>Delete</button>
+                <Link to="edit">Edit</Link>
+              </nav>
+            )}
+          </header>
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time
+                  dateTime={`Todo-DateT$Todo-Time`}
+                >{`${data.date} @ ${data.time}`}</time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }
--- 24-tanstack-query/src/utils/http.js
+++ 24-tanstack-query/src/utils/http.js
@@ -59,3 +59,39 @@ export async function fetchEventImages({ signal }) {
   const { images } = await response.json();
   return images;
 }
+
+export async function fetchSingleEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal: signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error(
+      "An error occurred while fetching the requested event"
+    );
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+  return event;
+}
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    const error = new Error(
+      "An error occurred while deleting the requested event"
+    );
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { message } = await response.json();
+  return message;
+}
